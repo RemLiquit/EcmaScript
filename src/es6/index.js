b27019a7abeda77754249ac3c9511fee2d53c450
RemLiquit <ceixpp@gmail.com>
2020-09-21 22:47:26 -0400

    Let y Const,Multilinea, Spread Operator y desestructuracion

--- src/es6/index.js
+++ src/es6/index.js
@@ -19,3 +19,47 @@ let epicPhrase = hello + " " + world;
 console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+let lorem =
+  "Qui consequatur. Commodi. Ipsum vel duis \n" +
+  "otra frase epica que necesitamos.";
+
+// es6
+
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+  name: "Diego",
+  age: 28,
+  country: "Bo",
+};
+
+console.log(person.name, person.age);
+
+let { name, age } = person;
+console.log(name, age);
+
+let team1 = ["Diego", "Julian", "Ricardo"];
+let team2 = ["Valeria", "Jessica", "Camila"];
+
+let education = ["David", ...team1, ...team2];
+
+console.log(education);
+
+{
+  var globalVar = "Global var";
+}
+
+{
+  let globalLet = "Global let";
+  console.log(globalLet);
+}
+
+console.log(globalVar);
+
+const a = "b";
