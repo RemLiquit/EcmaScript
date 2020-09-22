1f07697de4384b9cd6f990dea9114aa263c59e7d
RemLiquit <ceixpp@gmail.com>
2020-09-22 16:52:32 -0400

    Que se implemento en ES10

--- src/es9/index.js
+++ src/es9/index.js
@@ -37,7 +37,7 @@ helloWorld()
 
 const regexData = /([0-9]{4}-([0-9]{2}-[0-9]{2}))/;
 const match = regexData.exec("2018-04-20");
-const year = match[0];
+const year = match[1];
 const month = match[2];
 const day = match[3];
 
