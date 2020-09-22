e49b0c293cace2b81cdc8527bdf1e72341bf1169
RemLiquit <ceixpp@gmail.com>
2020-09-22 00:17:57 -0400

    Que se implemento en ES8

---
+++ src/es8/index.js
@@ -0,0 +1,24 @@
+const data = {
+  frontend: "Oscar",
+  backend: "Isabel",
+  desing: "Ana",
+};
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.lenght);
+
+const data = {
+  frontend: "Oscar",
+  backend: "Isabel",
+  desing: "Ana",
+};
+
+const values = Object.values(data);
+console.log(values);
+console.log(values.lenght);
+
+const string = "hello";
+console.log(string.padStart(7, "hi"));
+console.log(string.padEnd(12, " ----------"));
+console.log("food".padEnd(12, " --------"));
