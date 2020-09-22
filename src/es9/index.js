1d85ea9534ed283a516bfa0ee0603727b1f455d7
RemLiquit <ceixpp@gmail.com>
2020-09-22 13:19:18 -0400

    Que se implemento en ES9

---
+++ src/es9/index.js
@@ -0,0 +1,45 @@
+const obj = {
+  name: "Diego",
+  age: 28,
+  country: "BO",
+};
+
+// let { name, ...all } = obj;
+// console.log(name, all);
+
+let { country, ...all } = obj;
+console.log(all);
+
+const obj = {
+  name: "Diego",
+  age: 28,
+};
+
+const obj1 = {
+  ...obj,
+  country: "BO",
+};
+
+console.log(obj1);
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setInterval(() => resolve("Hello World"), 3000)
+      : reject(new Error("Test Error"));
+  });
+};
+
+helloWorld()
+  .then((response) => console.log(response))
+  .then((reject) => console.log(error))
+  .finally(() => console.log("Finalizo"));
+
+const regexData = /([0-9]{4}-([0-9]{2}-[0-9]{2}))/;
+const match = regexData.exec("2018-04-20");
+const year = match[0];
+const month = match[2];
+const day = match[3];
+
+console.log(year);
+console.log(year, month, day);
